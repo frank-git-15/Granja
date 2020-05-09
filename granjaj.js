40aa2a681a90e089ddc04f274d2bf98ff804154c
Francisco Alvarado <franciscoalvarrera15@gmail.com>
2020-05-09 14:43:36 -0500

    Se cargo la imagen de una granja al camvas

--- granjaj.js
+++ granjaj.js
@@ -2,9 +2,24 @@ var g=document.getElementById('granja');
 var lienzo=g.getContext('2d');
 console.log(lienzo);
 
-lienzo.beginPath();
-lienzo.strokeStyle="red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+var fondo=
+{
+  url:"granja.png",
+  carga:false
+}
+
+//Aqui se genera un variable tipo Image y se guarda logicamente en el vector JSON
+//y con esto podemos ubicar la imagen de fondo invocando fondo.imagen
+fondo.imagen=new Image();
+fondo.imagen.src=fondo.url;
+fondo.imagen.addEventListener("load",cargarFondo);
+
+function cargarFondo()
+{
+    fondo.carga=true
+    dibujar()
+}
+function dibujar()
+{
+  lienzo.drawImage(fondo.imagen,0,0);
+}
