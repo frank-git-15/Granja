c8163c939b87dac8c6a8f5904bb5186a536703d8
Francisco Alvarado <franciscoalvarrera15@gmail.com>
2020-05-09 15:01:08 -0500

    Agregue a la granja las imagenes de la vaca y el pollo en posiciones estaticas

--- granjaj.js
+++ granjaj.js
@@ -12,6 +12,16 @@ var cerdo=
   url:"cerdo.png",
   carga:false
 }
+var pollo=
+{
+  url:"pollo.png",
+  carga:false
+}
+var vaca=
+{
+  url:"vaca.png",
+  carga:false
+}
 
 //Aqui se genera un variable tipo Image y se guarda logicamente en el vector JSON
 //y con esto podemos ubicar la imagen de fondo invocando fondo.imagen
@@ -23,6 +33,14 @@ cerdo.imagen=new Image();
 cerdo.imagen.src=cerdo.url;
 cerdo.imagen.addEventListener('load',cargarCerdo);
 
+vaca.imagen=new Image();
+vaca.imagen.src=vaca.url;
+vaca.imagen.addEventListener('load',cargarVaca);
+
+pollo.imagen=new Image();
+pollo.imagen.src=pollo.url;
+pollo.imagen.addEventListener('load',cargarPollo);
+
 function cargarFondo()
 {
     fondo.carga=true;
@@ -31,6 +49,16 @@ function cargarFondo()
 function cargarCerdo()
 {
     cerdo.carga=true;
+    dibujar();
+}
+function cargarVaca()
+{
+    vaca.carga=true;
+    dibujar();
+}
+function cargarPollo()
+{
+    pollo.carga=true;
     dibujar();
 }
 function dibujar()
@@ -43,5 +71,14 @@ function dibujar()
   {
     lienzo.drawImage(cerdo.imagen,100,200);
   }
+  if(pollo.carga)
+  {
+    lienzo.drawImage(pollo.imagen,150,200);
+  }
+  if(vaca.carga)
+  {
+    lienzo.drawImage(vaca.imagen,200,200);
+  }
+
 
 }
