ffc188335ad5805dd7097dc2046f35146219b48e
Francisco Alvarado <franciscoalvarrera15@gmail.com>
2020-05-09 14:57:04 -0500

    Agregue un cerdo a la granja

--- granjaj.js
+++ granjaj.js
@@ -7,6 +7,11 @@ var fondo=
   url:"granja.png",
   carga:false
 }
+var cerdo=
+{
+  url:"cerdo.png",
+  carga:false
+}
 
 //Aqui se genera un variable tipo Image y se guarda logicamente en el vector JSON
 //y con esto podemos ubicar la imagen de fondo invocando fondo.imagen
@@ -14,12 +19,29 @@ fondo.imagen=new Image();
 fondo.imagen.src=fondo.url;
 fondo.imagen.addEventListener("load",cargarFondo);
 
+cerdo.imagen=new Image();
+cerdo.imagen.src=cerdo.url;
+cerdo.imagen.addEventListener('load',cargarCerdo);
+
 function cargarFondo()
 {
-    fondo.carga=true
-    dibujar()
+    fondo.carga=true;
+    dibujar();
+}
+function cargarCerdo()
+{
+    cerdo.carga=true;
+    dibujar();
 }
 function dibujar()
 {
-  lienzo.drawImage(fondo.imagen,0,0);
+  if(fondo.carga)
+  {
+    lienzo.drawImage(fondo.imagen,0,0);
+  }
+  if(cerdo.carga)
+  {
+    lienzo.drawImage(cerdo.imagen,100,200);
+  }
+
 }
